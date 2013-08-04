4f0e71ec05624c0ad5a42369bd57a8d8f878bb08
Caleb Penney <caleb.penney.75@gmail.com>
2013-08-04 14:08:59 -0500

    Fixed bug with user profile name validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -35,4 +35,12 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Caleb', last_name: 'Penney', email: 'caleb_penney_75@hotmail.com')
+    user.password = user.password_confirmation = 'password'
+
+    user.profile_name = 'bigcpenney_75'
+    assert user.valid?
+  end
 end
